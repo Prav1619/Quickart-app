cd9d41944c190420d161610d5e5fae4c2f168887
Prav1619 <vpravallikha@gmail.com>
2026-02-25 12:48:06 +0530

    Implement ProductController to manage products

---
+++ product-service/src/main/java/com/app/product/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.app.product.controller;
+
+import com.app.product.model.Product;
+import com.app.product.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return service.getProducts();
+    }
+}
