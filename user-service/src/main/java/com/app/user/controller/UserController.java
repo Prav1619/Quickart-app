f8fe7502e128babe704305c7180a0124f14df56b
Prav1619 <vpravallikha@gmail.com>
2026-02-25 13:06:44 +0530

    Implement UserController to manage user requests

---
+++ user-service/src/main/java/com/app/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.app.user.controller;
+
+import com.app.user.model.User;
+import com.app.user.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return service.getUsers();
+    }
+}
