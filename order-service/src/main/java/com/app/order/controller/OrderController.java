2d671f19e5e4fd4f17a054d82393f7a9f512f0af
Prav1619 <vpravallikha@gmail.com>
2026-02-25 12:55:14 +0530

    Implement OrderController to manage orders

---
+++ order-service/src/main/java/com/app/order/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.app.order.controller;
+
+import com.app.order.model.Order;
+import com.app.order.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService service;
+
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return service.getOrders();
+    }
+}
