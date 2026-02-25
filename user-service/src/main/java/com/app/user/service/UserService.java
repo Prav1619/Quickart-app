6f4dfd3630a5f30b652bd829f6e636fd2389da8a
Prav1619 <vpravallikha@gmail.com>
2026-02-25 13:06:06 +0530

    Add UserService to manage user data

---
+++ user-service/src/main/java/com/app/user/service/UserService.java
@@ -0,0 +1,16 @@
+package com.app.user.service;
+
+import com.app.user.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    public List<User> getUsers() {
+        return List.of(
+                new User("101", "Pravallikha"),
+                new User("102", "Kiran")
+        );
+    }
+}
