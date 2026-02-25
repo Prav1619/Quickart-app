de03f57b76e335b5ffeff572cd90e7528d64633d
Prav1619 <vpravallikha@gmail.com>
2026-02-25 13:05:03 +0530

    Add User model class with id and name fields

---
+++ user-service/src/main/java/com/app/user/model/User.java
@@ -0,0 +1,13 @@
+package com.app.user.model;
+
+public class User {
+    private String id;
+    private String name;
+
+    public User(String id, String name) {
+        this.id = id; this.name = name;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+}
