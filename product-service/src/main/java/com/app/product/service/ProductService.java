e1fb02dd70e364be9916e5b604582ab0f12ebb15
Prav1619 <vpravallikha@gmail.com>
2026-02-25 12:51:38 +0530

    Implement ProductService to return product list

---
+++ product-service/src/main/java/com/app/product/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.app.product.service;
+
+import com.app.product.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<Product> getProducts() {
+        return List.of(
+                new Product("1", "Laptop", 60000),
+                new Product("2", "Keyboard", 1500),
+                new Product("3", "Mouse", 800)
+        );
+    }
+}
