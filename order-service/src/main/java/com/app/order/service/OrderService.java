c73aa6eb8ec017cee8f7b920d071c1a422e92cc4
Prav1619 <vpravallikha@gmail.com>
2026-04-24 16:38:16 +0530

    Fix order quantity for order 6002

--- order-service/src/main/java/com/app/order/service/OrderService.java
+++ order-service/src/main/java/com/app/order/service/OrderService.java
@@ -10,7 +10,7 @@ public class OrderService {
     public List<Order> getOrders() {
         return List.of(
                 new Order("6001", "101", "1"),
-                new Order("6002", "102", "2")
+                new Order("6002", "102", "3")
         );
     }
 }
