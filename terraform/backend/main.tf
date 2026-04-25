3904b18ab1da2981144a017fc1736353b6f52d92
Prav1619 <vpravallikha@gmail.com>
2026-04-25 21:19:07 +0530

    Update main.tf

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -1,1 +1,14 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-state-bucket"
+}
 
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
