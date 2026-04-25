ad008be171d3abaafe04fc081c48993a3313369c
Prav1619 <vpravallikha@gmail.com>
2026-04-25 22:22:20 +0530

    Create IAM role for EKS cluster
    Add IAM role for EKS cluster with assume role policy.

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,3 +1,18 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.cluster_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
