e077c7eeddd08ae7650bc904d2d2f6423a5c4ce6
Prav1619 <vpravallikha@gmail.com>
2026-04-25 22:14:49 +0530

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,1 +1,19 @@
+variable "cidr_block" {
+  type = string
+}
 
+variable "environment" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
