a5a7a599b63ca7d28c9cad1a3f38ecc61989be92
Prav1619 <vpravallikha@gmail.com>
2026-04-25 21:28:58 +0530

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region to deploy EKS"
-  default     = "ap-south-1"
-}
-
-variable "cluster_name" {
-  description = "EKS Cluster Name"
-  default     = "quickart-eks"
-}
