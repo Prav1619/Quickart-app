d8e86bcb1a661e36f041af15653ea661231fad2a
Prav1619 <vpravallikha@gmail.com>
2026-04-25 22:26:54 +0530

    Add AWS provider and modules for VPC, EKS, and S3

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,1 +1,27 @@
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source = "../../modules/vpc"
+
+  cidr_block = "10.0.0.0/16"
+  environment = var.environment
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name = "dev-eks"
+  subnet_ids   = module.vpc.private_subnet_ids
+}
+
+module "s3" {
+  source = "../../modules/s3"
+
+  bucket_name = "dev-app-bucket-unique-123"
+}
