9077299d30266f8690c56251c6cd99f69d2af74a
Prav1619 <vpravallikha@gmail.com>
2026-05-08 12:37:14 +0530

    Update product list to include a mobile phone

--- product-service/src/main/java/com/app/product/service/ProductService.java
+++ product-service/src/main/java/com/app/product/service/ProductService.java
@@ -12,7 +12,7 @@ public class ProductService {
         return List.of(
                 new Product("1", "Laptop", 60000),
                 new Product("2", "Keyboard", 2500),
-                new Product("3", "Mouse", 800)
+                new Product("3", "mobile", 10000)
         );
     }
 }
