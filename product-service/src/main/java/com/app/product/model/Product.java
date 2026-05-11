90ce22262b93b3004251bc7c2c9030722e41081d
Prav1619 <vpravallikha@gmail.com>
2026-05-11 22:38:29 +0530

    Fix getter method to return 'Name' instead of 'name'

--- product-service/src/main/java/com/app/product/model/Product.java
+++ product-service/src/main/java/com/app/product/model/Product.java
@@ -11,6 +11,6 @@ public class Product {
 
     // getters
     public String getId() { return id; }
-    public String getName() { return name; }
+    public String getName() { return Name; }
     public int getPrice() { return price; }
 }
